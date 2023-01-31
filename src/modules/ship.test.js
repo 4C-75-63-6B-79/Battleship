6647e2897d98019f613f677a30ddd5eca3415c61
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-01-31 13:46:01 +0530

    added the test to check the ship hit function

--- src/modules/ship.test.js
+++ src/modules/ship.test.js
@@ -5,4 +5,11 @@ import Ship from "./ship";
 test("Ship creates a object of type ship", () => {
     const testShip = new Ship("test", 2);
     expect(Object.prototype.isPrototypeOf.call(Ship.prototype, testShip)).toBe(true);
+});
+
+test("Ship is hit increse the hit count", () => {
+    const testShip = new Ship("test", 2);
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.hitCount).toBe(2);
 })
