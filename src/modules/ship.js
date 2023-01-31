e642f9f25e821219926d7cf214f5adba3685867c
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-01-31 13:57:59 +0530

    added the issunk function to pass test

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -2,9 +2,12 @@ export default function Ship(name, length) {
     this.name = name;
     this.length = length;
     this.hitCount = 0;
-    this.sunk = false;
 }
 
 Ship.prototype.hit = function hit() {
     this.hitCount += 1;
+}
+
+Ship.prototype.isSunk = function isSunk() {
+    return (this.hitCount === this.length);
 }
