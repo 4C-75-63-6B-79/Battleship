9ad8264db55e3e3d78bd081f2986c7b00bd3e7fe
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-02-04 12:49:08 +0530

    place ship returns true if ship placed successfully else false

--- src/modules/friendlyWater.test.js
+++ src/modules/friendlyWater.test.js
@@ -3,10 +3,10 @@
 import FriendlyWater from "./friendlyWater";
 import Ship from "./ship";
 
-test("function place ship places ship at given coordinates", () => {
+test("function place ship places ship at given coordinates and returns true if ship placed else false", () => {
     const testFriendlyWater = new FriendlyWater();
     const testShip = new Ship("destroyer", 2);
-    testFriendlyWater.placeShip(testShip, [[0,0], [0,1]]);
+    expect(testFriendlyWater.placeShip(testShip, [[0,0], [0,1]])).toBe(true);
     expect(testFriendlyWater.board[0][0] === testShip).toBe(true);
     expect(testFriendlyWater.board[0][1] === testShip).toBe(true);
 });
