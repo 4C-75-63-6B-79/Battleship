1aa05b11e38100a6ec734e0bdc33c78252ffb72b
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-02-05 11:21:39 +0530

    created function to add the header content and the footer content

--- src/modules/first_page.js
+++ src/modules/first_page.js
@@ -1,14 +1,20 @@
-const firstPage = (function() {
+import odinLogo from "../assets/images/odin_logo.svg";
+
+const firstPage = (function initFirstPage() {
 
     function makeHeader() {
         const body = document.querySelector("body");
         const header = document.createElement("header");
+        body.appendChild(header);
+    }
+
+    function makeHeaderContent() {
+        const header = document.querySelector("header");
         const h1 = document.createElement("h1");
         h1.textContent = "BATTLESHIP";
         h1.setAttribute("class", "firstPage");
         h1.setAttribute("title", "BATTLESHIP");
         header.appendChild(h1);
-        body.appendChild(header);
     }
 
     function makeMain() {
@@ -23,10 +29,28 @@ const firstPage = (function() {
         body.appendChild(footer); 
     }
 
+    function createFooterContent() {
+        const footer = document.querySelector("footer");
+        const para = document.createElement("p");
+        para.textContent = "Created to complete the assignment of The Odin Project.";
+
+        // const linkToOdinProject = document.createElement("a");  // create a link to the odin project website
+        // linkToOdinProject.setAttribute("herf", "https://www.theodinproject.com/");
+        // const odinLogoImg = new Image();  // creating the logo of the odin project
+        // odinLogoImg.setAttribute("title", "The Odin Project Logo");
+        // odinLogoImg.src = odinLogo;
+
+        // linkToOdinProject.appendChild(odinLogoImg);
+        // para.appendChild(linkToOdinProject);
+        footer.appendChild(para);
+    }
+
     function startFirstPage() {
         makeHeader();
+        makeHeaderContent();
         makeMain();
         makeFooter();
+        createFooterContent();
     }
 
     return {
