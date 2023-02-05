70ff1d02acc7ff0bf983c2026b6f3fb7c53b7b2f
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-02-05 10:24:42 +0530

    created the gameControl with 2 players in it and create the initPlayer function

--- src/modules/game_control.js
+++ src/modules/game_control.js
@@ -1,54 +1,76 @@
-function randomShipCoordGenerator(numOfCoordinates) {
-    function getRandomCoords() {
-        return [Math.floor(Math.random()*10), Math.floor(Math.random()*10)];
+import Player from "./player";
+
+const gameControl = (function gameControl() {
+
+    let player1; 
+    let player2;
+
+    function initPlayers() {
+        player1 = new Player("user");
+        player2 = new Player("computer");
+    }
+    
+    function randomShipCoordGenerator(numOfCoordinates) {
+        function getRandomCoords() {
+            return [Math.floor(Math.random()*10), Math.floor(Math.random()*10)];
+        }
+    
+        function coordsValid(coord) {
+            return (coord[0] >= 0 && coord[0] <= 9 && coord[1] >= 0 && coord[1] <= 9);
+        }
+    
+        const coords = [getRandomCoords()];
+        const direction = Math.floor(Math.random() * 2); // 0 for x and 1 for y
+    
+        while(coords.length < numOfCoordinates) {
+            let first = coords[0];
+            let last = coords[coords.length - 1];
+    
+            if(direction === 0) {
+                first = [first[0],first[1]-1];
+                last = [last[0],last[1]+1];
+            } else if(direction === 1) {
+                first = [first[0]-1,first[1]];
+                last = [last[0]+1,last[1]];
+            }
+    
+            if(coordsValid(first)) {
+                coords.unshift(first);
+            }
+            if(coords.length === numOfCoordinates) break;
+            if(coordsValid(last)) {
+                coords.push(last);
+            }
+        }
+        return coords;
+    }
+    
+    function placePlayerShips(player) {   
+        function placeShip(name, len) {
+            let coords = randomShipCoordGenerator(len);
+            let isPlacedShip = player.placeShip(name, coords);
+            while(!isPlacedShip) {
+                coords = randomShipCoordGenerator(len);
+                isPlacedShip = player.placeShip(name, coords);
+            }
+        }
+    
+        const ships = [ 
+            ["carrier", 5],
+            ["battleship", 4],
+            ["cruiser", 3],
+            ["submarine", 3],
+            ["destroyer", 2],
+         ];
+         ships.forEach(ship => placeShip(ship[0], ship[1]));
     }
 
-    function coordsValid(coord) {
-        return (coord[0] >= 0 && coord[0] <= 9 && coord[1] >= 0 && coord[1] <= 9);
+    return {
+        placePlayerShips,
     }
+})();
 
-    const coords = [getRandomCoords()];
-    const direction = Math.floor(Math.random() * 2); // 0 for x and 1 for y
+const {placePlayerShips} = gameControl;
 
-    while(coords.length < numOfCoordinates) {
-        let first = coords[0];
-        let last = coords[coords.length - 1];
+export default  placePlayerShips ;
 
-        if(direction === 0) {
-            first = [first[0],first[1]-1];
-            last = [last[0],last[1]+1];
-        } else if(direction === 1) {
-            first = [first[0]-1,first[1]];
-            last = [last[0]+1,last[1]];
-        }
-
-        if(coordsValid(first)) {
-            coords.unshift(first);
-        }
-        if(coords.length === numOfCoordinates) break;
-        if(coordsValid(last)) {
-            coords.push(last);
-        }
-    }
-    return coords;
-}
-
-export default function placePlayerShips(player) {   
-    function placeShip(name, len) {
-        let coords = randomShipCoordGenerator(len);
-        let isPlacedShip = player.placeShip(name, coords);
-        while(!isPlacedShip) {
-            coords = randomShipCoordGenerator(len);
-            isPlacedShip = player.placeShip(name, coords);
-        }
-    }
-
-    const ships = [ 
-        ["carrier", 5],
-        ["battleship", 4],
-        ["cruiser", 3],
-        ["submarine", 3],
-        ["destroyer", 2],
-     ];
-     ships.forEach(ship => placeShip(ship[0], ship[1]));
-}
