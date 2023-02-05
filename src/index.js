36b65303b1e0d618f908471440497f99de93c461
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-02-05 13:31:28 +0530

    create objects folder and tested few object functions using index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import "./style.css";
 
+import Player from "./modules/object/player";
+import placePlayerShips from "./modules/object/game_control";
+import startFirstPage from "./modules/first_page";
+
 console.log("battleship");
+
+const test = new Player("test");
+(placePlayerShips(test));
+console.log(test.friendlyWater.board);
+startFirstPage();
