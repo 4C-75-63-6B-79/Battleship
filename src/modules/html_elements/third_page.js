0d670aaa4113a7bcd933f89f541111e04fe7588c
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-02-08 13:02:00 +0530

    created third page which removes the second page main content and then add the status box and the friendly Water grid from second page and removes all event listener form the box

---
+++ src/modules/html_elements/third_page.js
@@ -0,0 +1,60 @@
+import { makeElement } from "./html_create_functions";
+
+const thirdPage = (function initThirdPage() {
+
+    function removeAllContentMain() {
+        const main = document.querySelector("main");
+        main.replaceChildren();
+    }
+
+    function makeStatusBox() {
+        const main = document.querySelector("main");
+        const statusBox = makeElement({id:"statusBox", textContent: "Awaiting Order Chief", title: "Awaiting Order Chief"});
+        main.appendChild(statusBox);
+    }
+
+    function makeWaterContainers() {
+        const main = document.querySelector("main");
+        const waterContainer = makeElement({id: "waterContainer"});
+        
+        const friendlyWater = makeElement({id: "friendlyWater", classNames: "water"});
+        const enemyWater = makeElement({id: "enemyWater", classNames: "water"});
+
+        waterContainer.appendChild(friendlyWater);
+        waterContainer.appendChild(enemyWater);
+
+        main.appendChild(waterContainer);
+    }
+
+    function makeFriendlyWater(friendlyWaterGird) {
+        const friendlyWater = document.getElementById("friendlyWater");
+        const title = makeElement({elementType: "h2", textContent: "Friendly Waters", title: "Friendly Waters"});
+        friendlyWater.appendChild(title);
+        friendlyWater.appendChild(friendlyWaterGird);
+    }
+
+    function removeEventListenerFromGird(grid) {
+        const boxes = Array.from(grid.children);
+        boxes.forEach(box => {
+            box.replaceWith(box.cloneNode(true));
+        });
+    }
+
+    function loadThirdPage() {
+        const friendlyWaterGrid = document.querySelector("div.gridContainer");
+        removeEventListenerFromGird(friendlyWaterGrid);
+        removeAllContentMain();
+        makeStatusBox();
+        makeWaterContainers();
+        makeFriendlyWater(friendlyWaterGrid);
+    }
+
+    return {
+        loadThirdPage,
+    }
+
+})();
+
+const { loadThirdPage } = thirdPage;
+
+export default loadThirdPage;
