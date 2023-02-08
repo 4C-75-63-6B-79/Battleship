3a38f794391d8360f6a482cef60aeda7ac07c8ea
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-02-08 13:53:19 +0530

    only sending the callbackfunctions

--- src/modules/html_elements/second_page.js
+++ src/modules/html_elements/second_page.js
@@ -178,7 +178,7 @@ const secondPage = (function initSecondPage() {
 
     function makeGridContainer() {  // making gird to place the ships
         const main = document.querySelector("main");
-        const grid = makeGrid(null, [mouseOverBox, mouseOutBox, boxClicked]);
+        const grid = makeGrid([mouseOverBox, mouseOutBox, boxClicked]);
         main.appendChild(grid);
     }
 
