e751dcf03c02a142a7214f520c7682a6e3b35f17
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-02-09 11:13:34 +0530

    index js lodas the firsts page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import "./style.css";
 
-import Player from "./modules/object/player";
-import placePlayerShips from "./modules/object/game_control";
-import startFirstPage from "./modules/first_page";
+import loadFirstPage from "./modules/html_elements/first_page";
 
 console.log("battleship");
 
-const test = new Player("test");
-(placePlayerShips(test));
-console.log(test.friendlyWater.board);
-startFirstPage();
+loadFirstPage();
