aabc65019f777532892871f72c408555afd8e16f
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-02-11 19:27:40 +0530

    functions playerComputerMakesMove and playerUserMakesMove now return the array of ships that are hit

--- src/modules/object/game_control.js
+++ src/modules/object/game_control.js
@@ -17,12 +17,14 @@ const gameControl = (function gameControl() {
     function playerUserMakesMove(coord) {
         const isPlayerHit = player2.receiveAttack(coord);
         player1.markAttack(isPlayerHit, coord);
+        const shipsThatAreHit = player2.getShipsThatAreHit();
         const isNewShipSunk = player2.isNewShipSunk();
         const isWinner = player2.allShipSunk();
         return {
             name: player1.name,
             coord,              // coordinates of the attack
             isPlayerHit,        // status if it was a successful attack or not
+            shipsThatAreHit,    // ships that are hit
             isNewShipSunk,      // is there any new ship which is sunk
             isWinner,           // has the game been won
         };
@@ -32,12 +34,14 @@ const gameControl = (function gameControl() {
         const coord = player2.makeAttack();
         const isPlayerHit = player1.receiveAttack(coord);
         player2.markAttack(isPlayerHit, coord);
+        const shipsThatAreHit = player1.getShipsThatAreHit();
         const isNewShipSunk = player1.isNewShipSunk();
         const isWinner = player1.allShipSunk();
         return {
             name: player2.name,
             coord,              // coordinates of the attack
             isPlayerHit,        // status if it was a successful attack or not
+            shipsThatAreHit,    // ships that are hit
             isNewShipSunk,      // is there any new ship which is sunk
             isWinner,           // has the game been won
         };
